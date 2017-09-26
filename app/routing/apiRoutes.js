188c9b4af4da3b595f1e033eea56dc6004743aa2
fneim <neimfargin@gmx.com.tr>
2017-09-25 21:39:36 -0700

    working on posting survey results

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,11 @@
 var friends = require("../data/friends");
 
 module.exports = function(app) {
+
   app.get("/api/friends", function (req, res) {
     res.json(friends);
   });
+
   app.post("/api/friends", function(req, res) {
     for(var i = 0; i < friends.scores.length; i++) {
       console.log(friends[i].scores);
